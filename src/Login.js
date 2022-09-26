1d0abf85012513acf6a02461dbb66fbe4031378c
CamilingJS <jonathancamiling@gmail.com>
2022-09-25 23:47:43 -0700

    dependency changes

--- src/Login.js
+++ src/Login.js
@@ -2,10 +2,8 @@ import { PrimaryButton, LoginInput } from "./StyledComponents";
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Input from '@mui/material/Input';
-import InputLabel from '@mui/material/InputLabel';
 import InputAdornment from '@mui/material/InputAdornment';
 import FormControl from '@mui/material/FormControl';
-import TextField from '@mui/material/TextField';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 
 
